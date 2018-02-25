bf77cb88ed76144a8044f608f18706defab68f65
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-02-25 15:35:51 -0600

    Add files via upload

---
+++ CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    private GameObject player;
+    private Vector3 cameraOffset;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cameraOffset = new Vector3(-1, 10, -3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + cameraOffset;
+	}
+}
