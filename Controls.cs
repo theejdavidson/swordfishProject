a42f6f71e9196aa09a4d7b929bd545b53499764d
edavidson2 <32180998+edavidson2@users.noreply.github.com>
2018-03-07 11:09:54 -0600

    Add files via upload
    fixed controls acceleration of rotation to work in both directions and restart once momentum is lost

--- Controls.cs
+++ Controls.cs
@@ -23,30 +23,27 @@ public class Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float startRot = 0.0f;
         playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")); //keeps player moving in 2 dimension
         playerVelocity = playerInput * playerSpeed; //players velocity when moved
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition); //makes character face mouse
         Plane ground = new Plane(Vector3.up, Vector3.zero);
-        //float rayLength;
-        // if (ground.Raycast(ray, out rayLength))
-        //{
-        //   Vector3 pointToLook = ray.GetPoint(rayLength);
-        //  transform.LookAt(new Vector3(pointToLook.x, pointToLook.y, pointToLook.z));
-        // }//gets character to follow mouse
 
         if (Input.GetMouseButton(0))//tracks left click mouse button
         {
             transform.Rotate(-Vector3.up * (RotateSpeed * Time.deltaTime));
+
+            if (RotateSpeed < 2000)
+                RotateSpeed += 2;
         }
-        if (RotateSpeed < 2000)
-            RotateSpeed+= 2;
-        if (Input.GetMouseButton(0) == false)
-            RotateSpeed = 200;
+        
         else if (Input.GetMouseButton(1))
         {
-            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * (RotateSpeed * Time.deltaTime));
+            if (RotateSpeed < 2000)
+                RotateSpeed += 2;
         }
+        if (Input.GetMouseButton(1) == false && Input.GetMouseButton(0) == false)
+            RotateSpeed = 200;
     }
     private void FixedUpdate()
     {
