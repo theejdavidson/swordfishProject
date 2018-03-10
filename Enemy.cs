aa54ad15b1eefc6f0359e63809bb9bbc1b124a53
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-03-10 13:15:13 -0600

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,20 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Enemy : MonoBehaviour
-{
-    private float speed = .02f;
+
+public class Enemy : MonoBehaviour {
+
+    private float speed = .08f;
     GameObject player;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
     }
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < 5)
+
+        if ((Vector3.Distance(transform.position, player.transform.position) < 40 ) && (Vector3.Distance(transform.position, player.transform.position) > 10))
         {
             transform.LookAt(player.transform);
             transform.Translate(speed, 0, speed);
         }
+        if((Vector3.Distance(transform.position, player.transform.position) < 10))
+        {
+            transform.Translate(-speed, 0, -speed);
+        }
+
     }
 }
