edbd13b64ecde73ca441cb90d40f679611b40482
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-03-12 13:57:05 -0500

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     private float speed = .08f;
-    GameObject player;
-
+    public GameObject player;
+    public GameObject bullet;
+    private float fireRate = 1f;
+    private float fireTime = 0.0f;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        bullet = GameObject.FindGameObjectWithTag("bullet");
     }
     void Update()
     {
@@ -24,5 +27,22 @@ public class Enemy : MonoBehaviour {
             transform.Translate(-speed, 0, -speed);
         }
 
+        if((Vector3.Distance(transform.position, player.transform.position) < 40) && (Vector3.Distance(transform.position, player.transform.position) > 10))
+        {
+            transform.LookAt(player.transform);
+            Shoot();
+        }
     }
+
+    void Shoot()
+    {
+        if (Time.time > fireTime)
+        {
+            fireTime = Time.time + fireRate;
+            var shot = (GameObject)Instantiate(bullet, transform.position , transform.rotation);
+            shot.GetComponent<Rigidbody>().velocity = shot.transform.forward * 5;
+            Destroy(shot, 5);
+        }
+    }
+
 }
