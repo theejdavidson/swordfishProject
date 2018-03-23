26d4c4b645e07d40db364a2e5a09508635fac3a5
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-03-23 13:33:33 -0500

    Update Dam.cs
    fixed collision detection with bullet

--- Dam.cs
+++ Dam.cs
@@ -1,23 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dam : MonoBehaviour {
-
+public class Dam : MonoBehaviour
+{
+    public int hp = 100;
+    public int damage = 50;
+    private void Start()
+    {
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
-        var hit = collision.gameObject;
-        var hp = hit.GetComponent<Health>();
-        if(hp != null)
+        if (collision.gameObject.tag == "bullet")
         {
-            hp.TakeDam(10);
+            hp -= 10;
         }
-
-        Destroy(gameObject)
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
