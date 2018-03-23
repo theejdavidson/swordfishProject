bb7e68b2e4d42fae1fd8245f7326622b890f1a33
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-03-23 13:34:28 -0500

    Update Enemy.cs
    can now kill enemies with sword collision

--- Enemy.cs
+++ Enemy.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-    private float speed = .08f;
+    private float speed = .13f;
     public GameObject player;
     public GameObject bullet;
     public GameObject enemy;
-    private float fireRate = 1f;
+    private float fireRate = .5f;
     private float fireTime = 0.0f;
     private float timer = 0f;
     private float spawn = 4f;
+    public int max = 0;
 
-  
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -24,28 +26,30 @@ public class Enemy : MonoBehaviour {
     void Update()
     {
 
-        if ((Vector3.Distance(transform.position, player.transform.position) < 40 ) && (Vector3.Distance(transform.position, player.transform.position) > 10))
+        if ((Vector3.Distance(transform.position, player.transform.position) < 70))
         {
             transform.LookAt(player.transform);
             transform.Translate(speed, 0, speed);
         }
-        if((Vector3.Distance(transform.position, player.transform.position) < 10))
+        if ((Vector3.Distance(transform.position, player.transform.position) < 15))
         {
             transform.Translate(-speed, 0, -speed);
         }
 
-        if ((Vector3.Distance(transform.position, player.transform.position) < 40) && (Vector3.Distance(transform.position, player.transform.position) > 10))
+        if ((Vector3.Distance(transform.position, player.transform.position) < 70) && (Vector3.Distance(transform.position, player.transform.position) > 5))
         {
             transform.LookAt(player.transform);
             Shoot();
+
         }
-       /* timer += Time.deltaTime;
-        if (timer >= spawn)
-        {
-            SpawnEnemy();
-            timer = 0f;
-        }
-       */ 
+         timer += Time.deltaTime;
+         if (timer >= spawn && max <2)
+         {
+
+             SpawnEnemy();
+             timer = 0f;
+         }
+        
     }
 
     void Shoot()
@@ -53,17 +57,23 @@ public class Enemy : MonoBehaviour {
         if (Time.time > fireTime)
         {
             fireTime = Time.time + fireRate;
-            var shot = (GameObject)Instantiate(bullet, transform.position , transform.rotation);
-            shot.GetComponent<Rigidbody>().velocity = shot.transform.forward * 5;
-            Destroy(shot, 5);
+            var shot = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
+            shot.GetComponent<Rigidbody>().velocity = shot.transform.forward;
+            Destroy(shot, 3);
         }
     }
 
-   /* void SpawnEnemy()
+     void SpawnEnemy()
+     {
+         var enemyClone = (GameObject)Instantiate(enemy, player.transform.position + Vector3.forward *20, transform.rotation);
+        max++;
+     }
+    private void OnCollisionEnter(Collision collision)
     {
-        var enemyClone = (GameObject)Instantiate(enemy, player.transform.position + Vector3.forward *10, transform.rotation);
-        
+        if (collision.gameObject.tag == "Sword")
+        {
+            Destroy(gameObject);
+        }
     }
-    */
 
 }
