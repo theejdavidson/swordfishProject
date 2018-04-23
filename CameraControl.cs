ef9f8acbb18901184807d991edf36d9008d64f91
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-04-23 11:21:38 -0500

    Update CameraControl.cs

--- CameraControl.cs
+++ CameraControl.cs
@@ -17,3 +17,38 @@ public class CameraControl : MonoBehaviour {
         transform.position = player.transform.position + cameraOffset;
 	}
 }
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    private GameObject player;
+    private Vector3 cameraOffset;
+    private float timer = 0f;
+    private Vector3 currentPosition;
+    private Vector3 n = new Vector3(0, 0, 0);
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        cameraOffset = new Vector3(-1, 38, -3);
+        transform.position = player.transform.position + cameraOffset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if((Vector3.Distance(transform.position, player.transform.position)) != (Vector3.Distance(cameraOffset, n)))
+        {
+            transform.Translate(transform.forward);
+        }
+        else
+        {
+            transform.position = player.transform.position + cameraOffset;
+        }
+
+    }
+
+
+*/
