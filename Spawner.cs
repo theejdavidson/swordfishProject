1851f2cc936cddd1a1514d1bd3b75c2164083a5e
harmeetsingh5 <35540363+harmeetsingh5@users.noreply.github.com>
2018-04-27 13:59:53 -0500

    Update Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -1,30 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Spawner : Enemy {
+public class Spawner : Enemy
+{
     public int max = 0;
     private float timer = 0f;
     private float spawn = 2f;
+    public static int wave;
+    
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         enemy = GameObject.FindGameObjectWithTag("Enemy");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        wave = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
         timer += Time.deltaTime;
-        if (timer >= spawn && max < 0)
+        if (timer >= spawn && max < 1)
         {
             SpawnEnemy();
             timer = 0f;
         }
+        
     }
 
     void SpawnEnemy()
     {
         var enemyClone = (GameObject)Instantiate(enemy, transform.position, transform.rotation);
         max++;
+        wave++;
     }
 
 }
